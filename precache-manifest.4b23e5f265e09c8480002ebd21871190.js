88ab2b0524843a0f54f3d5b1b259c7471e4aa626
Mohit09091999 <singlamohit540@gmail.com>
2020-09-24 19:28:32 +0530

    Updates

---
+++ precache-manifest.4b23e5f265e09c8480002ebd21871190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0715f8b05a6ec5da312ab086e472b185",
+    "url": "/bobble.ai-front-end/index.html"
+  },
+  {
+    "revision": "945ccb1f330fc372f605",
+    "url": "/bobble.ai-front-end/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9479ae89381c4e59de56",
+    "url": "/bobble.ai-front-end/static/css/main.0a25dd40.chunk.css"
+  },
+  {
+    "revision": "945ccb1f330fc372f605",
+    "url": "/bobble.ai-front-end/static/js/2.a97232ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bobble.ai-front-end/static/js/2.a97232ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9479ae89381c4e59de56",
+    "url": "/bobble.ai-front-end/static/js/main.0f70b624.chunk.js"
+  },
+  {
+    "revision": "89a413632b488c935cac",
+    "url": "/bobble.ai-front-end/static/js/runtime-main.54dc0124.js"
+  }
+]);
